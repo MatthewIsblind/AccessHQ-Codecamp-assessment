3e28446ebeea76812cca53ba806d363307040b17
unknown <matet2501@gmail.com>
2022-07-01 15:11:47 +1000

    add in some comments for what should have done and tought process

--- src/test/java/com/HQassessment/test/TestSuite.java
+++ src/test/java/com/HQassessment/test/TestSuite.java
@@ -68,6 +68,7 @@ public class TestSuite {
         ProductPage productPage = new ProductPage(driver);
         //the argument for this could be better. Didn't know how to properly access the text
         productPage.navigateMenuTab(p -> p.equalsIgnoreCase("local_drink"));
+        //The selector argument is so that it can be used later on if there are more tab pages like dessert
         productPage.orderItem("Espresso ThickShake",1, By.className("drink"));
 
         productPage.navigateMenuTab(p -> p.equalsIgnoreCase("local_pizza"));
@@ -76,6 +77,28 @@ public class TestSuite {
         Assertions.assertEquals(3,navbar.getProductCount());
     }
 
+    @Test
+    public void CheckOrderSubtotal() {
+        Navbar navbar = new Navbar(driver);
+        navbar.openMenuPage();
+
+
+        ProductPage productPage = new ProductPage(driver);
+        productPage.navigateMenuTab(p -> p.equalsIgnoreCase("local_drink"));
+        productPage.orderItem("Espresso ThickShake",1, By.className("drink"));
+
+        productPage.navigateMenuTab(p -> p.equalsIgnoreCase("local_pizza"));
+        productPage.orderItem("Margherita",2,By.className("pizza"));
+
+        //click on the order navigation
+        //navbar.clickOrderPageButton();
+
+
+        //assrtequal here to check subtotal. The margherita subtotal can be found using a seperate method.
+        // OrderPage class may have to be created to account for future methods that may be on the page
+        //Assertions.assertEquals(19.98,ProductPage.getSubtotal("margherita"));
+
+    }
 
     @AfterEach
     public void TearDown() {
